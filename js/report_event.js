86d4fb30786078aa65dd5194ca05bdf7bbc23a1f
wsseong <woo_seock@naver.com>
2022-01-07 19:24:05 +0900

    report미완성

--- js/report_event.js
+++ js/report_event.js
@@ -43,33 +43,13 @@ next_btn.onclick = () => {
 //다음 버튼 누를 경우 다음 div 오기 처음 박스
 
 //두번째 박스
-function bd_color(v) {
-    sb_ip[v].style.border = '2px solid #3182F6';
-}
-
-
-
-sb_ip[0].onclick = () => {
-    bd_color(0);
-}
-
-sb_ip[1].onclick = () => {
-    bd_color(1);
-}
-
-sb_ip[2].onclick = () => {
-    bd_color(2);
-}
-sb_ip[0].onfocus = () => {
-    bd_color(0);
-}
-
-sb_ip[1].onfocus = () => {
-    bd_color(1);
-}
-
-sb_ip[2].onfocus = () => {
-    bd_color(2);
+for (let i = 0; i < sb_ip.length; i++){
+    sb_ip[i].onclick = () => {
+        sb_ip[i].style.border = '2px solid #3182F6';
+    }
+    sb_ip[i].onfocus = () => {
+        sb_ip[i].style.border = '2px solid #3182F6';
+    }
 }
 
 sb_ip[0].onblur = () => {
@@ -104,18 +84,172 @@ sb_ip[2].onblur = () => {
 
 
 info_next_btn.onclick = () => {
-    display_content.style.display = 'none';
-    info_hidden_content.style.display = 'none';
-    report_hidden_content.style.display = 'block';
-    dp_num[0].innerHTML = '<i class="fas fa-check"></i>';
-    dp_num[1].innerHTML = '<i class="fas fa-check"></i>';
-    hd_text_wrapper[0].style.opacity = '0.7';
-    hd_text_wrapper[1].style.opacity = '0.7';
-    line_bar_dp_none[0].style.display = 'block';
-    line_bar_dp_none[1].style.display = 'block';
-    hd_text_wrapper[1].style.display = 'flex';
-    hd_text_wrapper[2].style.display = 'flex';
+    if (err_message[0].style.display == 'block') {
+        alert('이름을 확인해주세요');
+    } else if (err_message[1].style.display == 'block') {
+        alert('생년월일을 확인해주세요');
+    } else if (err_message[2].style.display == 'block') {
+        alert('휴대전화를 확인해주세요');
+    } else if ((sb_ip[0].value.length === 0) || (sb_ip[1].value.length === 0) || (sb_ip[2].value.length === 0)) { 
+        alert('입력란을 채워주세요');
+    }else {
+        display_content.style.display = 'none';
+        info_hidden_content.style.display = 'none';
+        report_hidden_content.style.display = 'block';
+        dp_num[0].innerHTML = '<i class="fas fa-check"></i>';
+        dp_num[1].innerHTML = '<i class="fas fa-check"></i>';
+        hd_text_wrapper[0].style.opacity = '0.7';
+        hd_text_wrapper[1].style.opacity = '0.7';
+        line_bar_dp_none[0].style.display = 'block';
+        line_bar_dp_none[1].style.display = 'block';
+        hd_text_wrapper[1].style.display = 'flex';
+        hd_text_wrapper[2].style.display = 'flex';
+    }
 }
 //두번째 박스
 
+//세번째 박스
+let select_box_controller = document.querySelectorAll('.select_box_controller');
+let dropbox_temp = document.querySelectorAll('.dropbox_temp');
+let s_d = document.querySelectorAll('.s_d');
+let s_d_ip = document.querySelector('.s_d_ip');
+let s_b = document.querySelectorAll('.s_b');
+let s_b_ip = document.querySelector('.s_b_ip');
+
+let ubn_ip = document.querySelector('.ubn_ip');
+let utd_ip = document.querySelector('.utd_ip');
+let rep_ip_chk = document.querySelectorAll('.rep_ip_chk');
+let money_count = document.querySelector('.money_count');
+
+
+
+select_box_controller[0].onclick = () => {
+    dropbox_temp[0].style.display = 'block';
+    select_box_controller[0].style.border = '2px solid #3182F6'
+    select_box_controller[0].onblur = () => {
+        setTimeout(function() {
+            dropbox_temp[0].style.display = 'none';
+          }, 500);
+    }
+}
+
+for (let i = 0; i < s_d.length; i++){
+    s_d[i].onclick = () => {
+        utd_ip.value = s_d[i].innerHTML;
+        s_d_ip.innerHTML = s_d[i].innerHTML;
+        select_box_controller[0].style.border = '1px solid #E6E9EC';
+        s_d_ip.style.opacity = '1';
+        dropbox_temp[0].style.display = 'none';
+    }
+}
+
+
+for (let i = 0; i < rep_ip_chk.length; i++){
+    rep_ip_chk[i].onclick = () => {
+        rep_ip_chk[i].style.border = '2px solid #3182F6';
+
+    }
+    rep_ip_chk[i].onfocus = () => {
+        rep_ip_chk[i].style.border = '2px solid #3182F6';
+
+    }
+
+    if (i == 0) {
+        rep_ip_chk[i].onblur = () => {
+            if (rep_ip_chk[i].value.length == 0) {
+                rep_ip_chk[i].style.border = '2px solid #EF5350';
+                err_message[4].style.display = 'block';
+            } else {
+                rep_ip_chk[i].style.border = '1px solid #E6E9EC';
+                err_message[4].style.display = 'none';
+            }
+        }
+        rep_ip_chk[i].onkeyup = () => {
+            function numberToKorean(number){
+                var inputNumber  = number < 0 ? false : number;
+                var unitWords    = ['', '만', '억', '조', '경'];
+                var splitUnit    = 10000;
+                var splitCount   = unitWords.length;
+                var resultArray  = [];
+                var resultString = '';
+            
+                for (let i = 0; i < splitCount; i++){
+                    let unitResult = (inputNumber % Math.pow(splitUnit, i + 1)) / Math.pow(splitUnit, i);
+                    unitResult = Math.floor(unitResult);
+                    if (unitResult > 0){
+                        resultArray[i] = unitResult;
+                    }
+                }
+            
+                for (var i = 0; i < resultArray.length; i++){
+                    if(!resultArray[i]) continue;
+                    resultString = String(resultArray[i]) + unitWords[i] + resultString;
+                }
+            
+                return resultString;
+            }
+            money_count.style.display = 'block';
+  
+            money_count.innerHTML = numberToKorean(rep_ip_chk[i].value) + "원"; 
+
+        }
+    } else if (i == 1) {
+        rep_ip_chk[i].onblur = () => {
+            if (rep_ip_chk[i].value.length < 8) {
+                rep_ip_chk[i].style.border = '2px solid #EF5350';
+                err_message[5].style.display = 'block';
+            } else {
+                rep_ip_chk[i].style.border = '1px solid #E6E9EC';
+                err_message[5].style.display = 'none';
+            }
+        }
+    } else if (i == 2) {
+        rep_ip_chk[i].onblur = () => {
+            if (rep_ip_chk[i].value.length == 0) {
+                rep_ip_chk[i].style.border = '2px solid #EF5350';
+                err_message[6].style.display = 'block';
+            } else {
+                rep_ip_chk[i].style.border = '1px solid #E6E9EC';
+                err_message[6].style.display = 'none';
+            }
+        }
+    } else {
+        rep_ip_chk[i].onblur = () => {
+            if (rep_ip_chk[i].value.length == 0) {
+                rep_ip_chk[i].style.border = '2px solid #EF5350';
+                err_message[8].style.display = 'block';
+            } else {
+                rep_ip_chk[i].style.border = '1px solid #E6E9EC';
+                err_message[8].style.display = 'none';
+            }
+        }
+    }
+}
+
+
+
+select_box_controller[1].onclick = () => {
+    dropbox_temp[1].style.display = 'block';
+    select_box_controller[1].style.border = '2px solid #3182F6'
+    select_box_controller[1].onblur = () => {
+        setTimeout(function() {
+            dropbox_temp[1].style.display = 'none';
+          }, 500);
+    }
+}
+
+
+
+for (let i = 0; i < s_b.length; i++){
+    s_b[i].onclick = () => {
+        ubn_ip.value = s_b[i].innerHTML;
+        s_b_ip.innerHTML = s_b[i].innerHTML;
+        select_box_controller[1].style.border = '1px solid #E6E9EC';
+        s_b_ip.style.opacity = '1';
+        dropbox_temp[1].style.display = 'none';
+    }
+}
+
+
+//세번째 박스
 
