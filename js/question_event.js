b339746a9550bd52cf44444867fabd9e562a8692
wsseong <woo_seock@naver.com>
2022-01-11 13:04:07 +0900

    헤더 약간 수정, content 작업중

---
+++ js/question_event.js
@@ -0,0 +1,26 @@
+let focus_list = document.querySelectorAll('.focus_list a');
+
+
+for (let i = 0; i < focus_list.length; i++){
+    function loopIf(num) {
+        if (i == num) {
+            focus_list[i].style.color = '#3182f6';
+        } else {
+            focus_list[i].style.color = '#8b98a1';
+        }
+    }
+    focus_list[i].onclick = () => {
+        loopIf(0);
+        loopIf(1);
+        loopIf(2);
+        loopIf(3);
+        loopIf(4);
+        loopIf(5);
+        loopIf(6);
+        loopIf(7);
+        loopIf(8);
+        loopIf(9);
+        loopIf(10);
+        loopIf(11);
+    }
+}
